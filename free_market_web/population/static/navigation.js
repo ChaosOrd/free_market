4c79d7596cb9ec255b9dd87a85e92f3277414a04
ChaosOrd <punkihoy86@gmail.com>
2015-01-16 02:07:38 +0200

    Fully implemented navigation script which switches to next text input when pressing enter

--- free_market_web/population/static/navigation.js
+++ free_market_web/population/static/navigation.js
@@ -1,22 +1,24 @@
-var Navigation = {
-    function IsTextbox(element)
+var navigation = {
+    IsTextBox: function (element)
     {
-        if (element.tagName && element.tagName.toLowerCase() == "input" && textbox.type.toLowerCase() == "textbox")
+        if (element.is('input') && element.prop('type') == "text")
         {
             return true;
         }
 
         return false;
     }
-}
+};
+
 
 $(document).ready(function () {
     document.onkeypress=function(e) {
         e = e || window.event;
         var selected_tab_idx = document.activeElement.tabIndex;
-        if (e.which == 13 )
+        if (e.which == 13 && navigation.IsTextBox($(document.activeElement)))
         {
             $('input[tabindex=' + (selected_tab_idx + 1) + ']').focus();
         }
     }
 });
+
