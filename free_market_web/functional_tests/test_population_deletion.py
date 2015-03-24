5fb711e83464df6356e11fee92f360a2261609ea
ChaosOrd <punkihoy86@gmail.com>
2015-03-24 22:15:40 +0200

    Finished writing the functional test for population deletion

--- free_market_web/functional_tests/test_population_deletion.py
+++ free_market_web/functional_tests/test_population_deletion.py
@@ -11,20 +11,14 @@ class CreatePopulationWithSupplyAndDemandTest(FunctionalTest):
         self._create_population_fixtures()
         self._create_supply_demand_fixtures()
 
-    def test_population_deletion(self):
-        # Uncle Bob decides to check out the previously created universe
-        universe_url = self.live_server_url + '/universe/1/'
-        self.browser.get(universe_url)
-
-        self.fail(msg='Continue the test')
-
     def _create_resource_fixtures(self):
         self.milk = Resource.objects.create(name='Milk')
         self.bread = Resource.objects.create(name='Bread')
         self.beer = Resource.objects.create(name='Beer')
+        self.vodka = Resource.objects.create(name='Vodka')
 
     def _create_universe_fixtures(self):
-        self.universe = Universe.objects.create()
+        self.universe = Universe.objects.create(id=0)
 
     def _create_population_fixtures(self):
         self.first_pop = Population.objects.create(universe=self.universe,
@@ -38,6 +32,48 @@ class CreatePopulationWithSupplyAndDemandTest(FunctionalTest):
         SupplyDemand.objects.create(population=self.first_pop,
                                     resource=self.bread, value=1.3)
         SupplyDemand.objects.create(population=self.second_pop,
-                                    resource=self.bread, value=-0.6)
+                                    resource=self.beer, value=-0.6)
         SupplyDemand.objects.create(population=self.second_pop,
-                                    resource=self.beer, value=2.5)
+                                    resource=self.vodka, value=2.5)
+
+    def test_population_deletion(self):
+        # Uncle Bob decides to check out the previously created universe
+        universe_url = self.live_server_url + '/population/universe/0/'
+        self.browser.get(universe_url)
+
+        body = self.browser.find_element_by_tag_name('body').text
+
+        # He sees the previously created first item
+        self.assertIn('Farmers', body)
+        self.assertIn('Milk', body)
+        self.assertIn('0.5', body)
+        self.assertIn('Bread', body)
+        self.assertIn('1.3', body)
+
+        # And the second item
+        self.assertIn('Miners', body)
+        self.assertIn('Beer', body)
+        self.assertIn('0.6', body)
+        self.assertIn('Vodka', body)
+        self.assertIn('2.5', body)
+
+        # There are 2 delete links, one for each population
+        delete_links = self.browser.find_elements_by_link_text('Delete')
+        self.assertEqual(len(delete_links), 2)
+
+        # He decides to delete the first population
+        delete_links[0].click()
+
+        # The first item does not exist
+        self.assertNotIn('Farmers', body)
+        self.assertNotIn('Milk', body)
+        self.assertNotIn('0.5', body)
+        self.assertNotIn('Bread', body)
+        self.assertNotIn('1.3', body)
+
+        # But the second item still present
+        self.assertIn('Miners', body)
+        self.assertIn('Beer', body)
+        self.assertIn('0.6', body)
+        self.assertIn('Vodka', body)
+        self.assertIn('2.5', body)
