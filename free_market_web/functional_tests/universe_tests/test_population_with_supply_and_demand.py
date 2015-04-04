ef93699867d40dde779c0590f3307118ef6dc750
ChaosOrd <punkihoy86@gmail.com>
2015-04-04 21:48:50 +0300

    All functional tests now pass after adding name to universe

--- free_market_web/functional_tests/universe_tests/test_population_with_supply_and_demand.py
+++ free_market_web/functional_tests/universe_tests/test_population_with_supply_and_demand.py
@@ -10,6 +10,29 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         Resource.objects.create(name='Bread')
         Resource.objects.create(name='Beer')
 
+    def select_sd_resource_name(self, name, sd_idx):
+        resource_cb = self.get_sd_resource_widget(sd_index=sd_idx)
+        self.select_listbox_item(resource_cb, name)
+
+    def set_sd_value(self, value, sd_idx):
+        sd_value_tb = self.get_sd_value_widget(sd_idx)
+        sd_value_tb.clear()
+        sd_value_tb.send_keys(value)
+
+    def get_sd_resource_widget(self, sd_index):
+        resource_element_id = self.get_sd_resource_id(sd_index)
+        return self.browser.find_element_by_id(resource_element_id)
+
+    def get_sd_value_widget(self, sd_index):
+        value_element_id = self.get_sd_value_id(sd_index)
+        return self.browser.find_element_by_id(value_element_id)
+
+    def get_sd_resource_id(self, sd_index):
+        return'id_sd_{}-resource'.format(sd_index)
+
+    def get_sd_value_id(self, sd_index):
+        return 'id_sd_{}-value'.format(sd_index)
+
     def test_can_create_a_population_with_demand_and_get_see_it_later(self):
         # Yulia have decided to create a universe with populations
         # that actually have consumption and production
@@ -20,10 +43,8 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         self.set_universe_name('Simple universe')
 
         # First she creates a population of people with "Food" demand
-        name_tb = self.browser.find_element_by_id('id_name')
-        name_tb.send_keys('People\n')
-        qty_tb = self.browser.find_element_by_id('id_quantity')
-        qty_tb.send_keys('20\n')
+        self.set_population_name('People')
+        self.set_population_quantity('20')
 
         # The supply demand input controls are not seen yet
         self.assert_element_does_not_exist(self.get_sd_value_id(0))
@@ -32,18 +53,11 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         # She clicks on add demand item
         self.browser.find_element_by_link_text('Add supply/demand').click()
 
-        # She chooses the conusmed resource and hits enter
-
-        resource_cb = self.get_sd_resource_widget(sd_index=0)
-        self.select_listbox_item(resource_cb, 'Milk')
-        resource_cb.send_keys('\n')
-
-        # The supply/demand value input box became focused
-        demand_val_tb = self.get_sd_value_widget(0)
-        self.assertEqual(self.browser.switch_to.active_element, demand_val_tb)
+        # She chooses the conusmed resource
+        self.select_sd_resource_name('Milk', 0)
 
         # She inserts the demand value
-        demand_val_tb.send_keys('-4.5\n')
+        self.set_sd_value('-4.5', 0)
 
         # The second supply demand controls are not seen yet
         self.assert_element_does_not_exist(self.get_sd_resource_id(1))
@@ -68,16 +82,10 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         self.browser.find_element_by_link_text('Add supply/demand').click()
 
         # She enteres the name of other and hits enter
-        resource_cb = self.get_sd_resource_widget(2)
-        self.select_listbox_item(resource_cb, 'Bread')
-        resource_cb.send_keys('\n')
-
-        # The supply/demand value input box became focused
-        supply_val_tb = self.get_sd_value_widget(2)
-        self.assertEqual(self.browser.switch_to.active_element, supply_val_tb)
+        self.select_sd_resource_name('Bread', 2)
 
         # She inserts the supply
-        supply_val_tb.send_keys('0.3\n')
+        self.set_sd_value('0.3', 2)
 
         # She hits the save button
         self.browser.find_element_by_id('id_save').click()
@@ -98,15 +106,15 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         self.browser.get(self.server_url)
         self.browser.find_element_by_link_text('New universe').click()
 
+        self.set_universe_name('Simple universe')
+
         # He types the population details
-        self.browser.find_element_by_id('id_name').send_keys('Clean coders\n')
-        input_qty_tb = self.browser.find_element_by_id('id_quantity')
-        input_qty_tb.send_keys('3\n')
+        self.set_population_name('Clean coders')
+        self.set_population_quantity('6')
 
         # He decides to add it a supply but forgets to choose a resource
         self.browser.find_element_by_link_text('Add supply/demand').click()
-        supply_val_tb = self.get_sd_value_widget(0)
-        supply_val_tb.send_keys('0.4')
+        self.set_sd_value('0.4', 0)
         self.browser.find_element_by_id('id_save').click()
 
         # He sees an informative error message
@@ -121,21 +129,19 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         # The population was successfully added
         text = self.browser.find_element_by_tag_name('body').text
         self.assertIn('Clean coders', text)
-        self.assertIn('3', text)
+        self.assertIn('6', text)
         self.assertIn('Bread', text)
         self.assertIn('0.4', text)
 
         # He decides to add another population
 
         # He types the population details
-        self.browser.find_element_by_id('id_name').send_keys('Code monkeys\n')
-        input_qty_tb = self.browser.find_element_by_id('id_quantity')
-        input_qty_tb.send_keys('6\n')
+        self.set_population_name('Code monkeys')
+        self.set_population_quantity('3')
 
         # He decides to add it a supply but forgets to insert a value
         self.browser.find_element_by_link_text('Add supply/demand').click()
-        resource_cb = self.get_sd_resource_widget(0)
-        self.select_listbox_item(resource_cb, 'Beer')
+        self.select_sd_resource_name('Beer', 0)
         self.browser.find_element_by_id('id_save').click()
 
         # He sees another error message
@@ -143,8 +149,7 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         self.assertIn('Supply/Demand value can not be empty', text)
 
         # He enters some junk and hits save
-        supply_val_tb = self.get_sd_value_widget(0)
-        supply_val_tb.send_keys('zero seven')
+        self.set_sd_value('zero seven', 0)
         self.browser.find_element_by_id('id_save').click()
 
         # The error message has changed
@@ -152,9 +157,7 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         self.assertIn('Supply/Demand value is invalid', text)
 
         # He rewrites value and hits save
-        supply_val_tb = self.get_sd_value_widget(0)
-        supply_val_tb.clear()
-        supply_val_tb.send_keys('0.7')
+        self.set_sd_value('0.7', 0)
         self.browser.find_element_by_id('id_save').click()
 
         # He sees the new population along with the old population data
@@ -167,17 +170,3 @@ class CreatePopulationWithSupplyAndDemandTest(UniverseTest):
         self.assertIn('6', text)
         self.assertIn('Bread', text)
         self.assertIn('0.7', text)
-
-    def get_sd_resource_widget(self, sd_index):
-        resource_element_id = self.get_sd_resource_id(sd_index)
-        return self.browser.find_element_by_id(resource_element_id)
-
-    def get_sd_value_widget(self, sd_index):
-        value_element_id = self.get_sd_value_id(sd_index)
-        return self.browser.find_element_by_id(value_element_id)
-
-    def get_sd_resource_id(self, sd_index):
-        return'id_sd_{}-resource'.format(sd_index)
-
-    def get_sd_value_id(self, sd_index):
-        return 'id_sd_{}-value'.format(sd_index)
