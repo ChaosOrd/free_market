694cb15eedcf796df48bebde78e1466973f57b15
ChaosOrd <punkihoy86@gmail.com>
2015-05-03 19:46:38 +0300

    Implemented basic exchange functionality. Added failing test that deals with orders sequce

--- simulation/exchange.py
+++ simulation/exchange.py
@@ -1,9 +1,28 @@
+from math import copysign
+
+
 class Exchange(object):
 
+    def __init__(self):
+        self.book = []
+
     def place_order(self, order):
-        order.sender.on_order_filled(order=order,
-                                     price=order.price,
-                                     quantity=order.quantity)
+        self.book.append(order)
+
+        self.book = sorted(self.book, key=lambda o: copysign(o.price, o.quantity))
+        self.__fill_orders_if_needed()
+
+    def __fill_orders_if_needed(self):
+        for idx in range(len(self.book)-1):
+            first_order = self.book[idx]
+            second_order = self.book[idx+1]
+            if (first_order.price >= second_order.price):
+                first_order.sender.on_order_filled(order=first_order,
+                                                   price=second_order.price,
+                                                   quantity=first_order.quantity)
+                second_order.sender.on_order_filled(order=second_order,
+                                                    price=second_order.price,
+                                                    quantity=second_order.quantity)
 
 
 class Order(object):
--- simulation/tests/test_exchange.py
+++ simulation/tests/test_exchange.py
@@ -5,20 +5,58 @@ from ..exchange import Exchange
 
 class ExchangeTest(TestCase):
 
+    def setUp(self):
+        self.buy_order = Mock()
+        self.buy_sender = self.buy_order.sender
+        self.sell_order = Mock()
+        self.sell_sender = self.sell_order.sender
+        self.buy_order.price = 10
+        self.buy_order.quantity = -5
+
+        self.sell_order.price = 10
+        self.sell_order.quantity = 5
+
     def test_orders_with_same_prices_result_in_fill(self):
         exchange = Exchange()
-        buy_order = Mock()
-        buy_order.price = 10
-        buy_order.quantity = -5
 
-        sell_order = Mock()
-        sell_order.price = 10
-        sell_order.quantity = 5
+        exchange.place_order(self.buy_order)
+        exchange.place_order(self.sell_order)
 
-        exchange.place_order(buy_order)
-        exchange.place_order(sell_order)
+        self.buy_sender.on_order_filled.assert_called_once_with(
+            order=self.buy_order, price=10, quantity=-5)
+        self.sell_sender.on_order_filled.assert_called_once_with(
+            order=self.sell_order, price=10, quantity=5)
 
-        buy_order.sender.on_order_filled.assert_called_once_with(
-            order=buy_order, price=10, quantity=-5)
-        sell_order.sender.on_order_filled.assert_called_once_with(
-            order=sell_order, price=10, quantity=5)
+    def test_orders_with_uncrossing_prices_do_not_result_in_fill(self):
+        exchange = Exchange()
+        self.sell_order.price = 11
+
+        exchange.place_order(self.buy_order)
+        exchange.place_order(self.sell_order)
+
+        self.assertFalse(self.buy_sender.on_order_filled.called)
+        self.assertFalse(self.sell_sender.on_order_filled.called)
+
+    def test_filled_with_sell_order_price_if_it_came_first(self):
+        exchange = Exchange()
+        self.sell_order.price = 9.5
+
+        exchange.place_order(self.sell_order)
+        exchange.place_order(self.buy_order)
+
+        self.buy_sender.on_order_filled.assert_called_once_with(
+            order=self.buy_order, price=9.5, quantity=-5)
+        self.sell_sender.on_order_filled.assert_called_once_with(
+            order=self.sell_order, price=9.5, quantity=5)
+
+    def test_filled_with_buy_order_price_if_it_came_first(self):
+        exchange = Exchange()
+        self.sell_order.price = 9.5
+
+        exchange.place_order(self.buy_order)
+        exchange.place_order(self.sell_order)
+
+        self.buy_sender.on_order_filled.assert_called_once_with(
+            order=self.buy_order, price=10, quantity=-5)
+        self.sell_sender.on_order_filled.assert_called_once_with(
+            order=self.sell_order, price=10, quantity=5)
